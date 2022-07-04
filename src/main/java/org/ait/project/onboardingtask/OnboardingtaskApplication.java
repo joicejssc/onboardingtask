4714d3c45a8b1e2b28e3b99afc5597ddd74cff41
Joice Jessica <joice.jessica@akarinti.tech>
2022-07-04 13:34:54 +0700

    Initial Commit

---
+++ src/main/java/org/ait/project/onboardingtask/OnboardingtaskApplication.java
@@ -0,0 +1,13 @@
+package org.ait.project.onboardingtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ait/project/onboardingtask/OnboardingtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ait.project.onboardingtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
